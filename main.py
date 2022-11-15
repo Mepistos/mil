dfb402b8dc6e1231f55d80ef2d8dc89e51492afe
Mepistos <mepistos@Mepistosui-MacBookPro.local>
2022-11-15 20:37:24 +0900

    tom

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-
-@app.rout("/hello/")
-def hello_flask():
-    return "Hello Flask!"
+    return render_template('main.html')
 
 if __name__ == "__main__":
     app.run()
